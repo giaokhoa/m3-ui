f91f32fc5c415256cafea205ad5e66a41974e311
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-08-21 12:33:56 +0700

    feat(tokens): add motion tokens

---
+++ packages/tokens/src/motion.ts
@@ -0,0 +1,16 @@
+export const motionEasing = {
+  standard: 'cubic-bezier(0.2, 0, 0, 1)',
+} as const;
+
+export const motionDurationMs = {
+  short1: 50,
+  short2: 100,
+  short3: 150,
+  short4: 200,
+  medium1: 250,
+  medium2: 300,
+  medium3: 350,
+  medium4: 400,
+  long1: 450,
+  long2: 500,
+} as const;
