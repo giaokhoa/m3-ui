d1877dfe741d9609a49b48b52b861e8a41ae334f
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-08-21 12:36:47 +0700

    feat(ripple): export primitive

---
+++ packages/ui/src/internal/ripple/index.ts
@@ -0,0 +1,3 @@
+export { Ripple, type RippleProps } from './Ripple';
+export { useRipple, type RippleController, type UseRippleOptions } from './useRipple';
+export type { RippleOrigin, RipplePointerType, RipplePressEvent, RippleWave } from './types';
