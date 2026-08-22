433adfd3fe757f0a730f8a60cf11e6ae8c8215a7
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-08-22 09:01:49 +0700

    fix(button): expose container radius to inset focus ring

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -153,6 +153,7 @@ function ButtonImpl({
         <>
           <Ripple
             controller={ripple}
+            focusRingRadius="var(--_button-container-radius)"
             isFocusVisible={renderProps.isFocusVisible}
             stateInteraction={latestButtonStateLayerInteraction(
               activeInteractions,
