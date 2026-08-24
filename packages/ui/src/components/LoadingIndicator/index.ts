e654867e5389c0c1bb60910bc7a22da8a11ce9ed
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-08-24 12:17:21 +0700

    feat(ui): export Material 3 Loading Indicator

---
+++ packages/ui/src/components/LoadingIndicator/index.ts
@@ -0,0 +1,10 @@
+export {
+  ContainedLoadingIndicator,
+  LoadingIndicator,
+  type ContainedLoadingIndicatorProps,
+  type LoadingIndicatorProps,
+} from './LoadingIndicator';
+export {
+  loadingIndicatorRuntime,
+  loadingIndicatorTokens,
+} from './LoadingIndicator.defaults';
