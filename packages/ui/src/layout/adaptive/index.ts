a43ec81fdfc3f2f7dd0ad2597b78a30c1960ec60
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-08-28 11:04:53 +0700

    feat: export three pane scaffold state

--- packages/ui/src/layout/adaptive/index.ts
+++ packages/ui/src/layout/adaptive/index.ts
@@ -3,5 +3,6 @@ export * from './paneExpansionState';
 export * from './paneMotion';
 export * from './paneScaffoldDirective';
 export * from './threePaneScaffold';
+export * from './threePaneScaffoldState';
 export * from './useWindowAdaptiveInfo';
 export * from './windowSizeClass';
