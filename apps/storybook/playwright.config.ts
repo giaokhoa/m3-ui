c3f0753a09cce6892910c4ff59b2fb00146a91d3
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-08-29 19:37:51 +0700

    ci(storybook): run visual tests with two workers

--- apps/storybook/playwright.config.ts
+++ apps/storybook/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 1 : 0,
-  workers: 1,
+  workers: process.env.CI ? 2 : undefined,
   reporter: process.env.CI ? 'github' : 'list',
   snapshotPathTemplate:
     '{testDir}/__screenshots__/{testFilePath}/{arg}{ext}',
