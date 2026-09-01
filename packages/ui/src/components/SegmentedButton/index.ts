41325c007b5fd5fdc5217d33a1e8fb5291339476
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-09-01 13:37:15 +0700

    refactor(segmented-button): remove static projection exports

--- packages/ui/src/components/SegmentedButton/index.ts
+++ packages/ui/src/components/SegmentedButton/index.ts
@@ -1,7 +1,1 @@
 export * from './SegmentedButton';
-export {
-  getSegmentedButtonDisabledColors,
-  segmentedButtonRowStyle,
-  segmentedButtonStyle,
-} from './SegmentedButton.defaults';
-export { segmentedButtonRuntime, segmentedButtonTokens } from './SegmentedButton.tokens';
