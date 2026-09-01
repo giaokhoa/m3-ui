e55e1dd38e045a576a870b82bc50bfd0e0ffa6d1
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-09-01 15:22:51 +0700

    refactor(split-button): stop exporting static projection tables

--- packages/ui/src/components/SplitButton/index.ts
+++ packages/ui/src/components/SplitButton/index.ts
@@ -1,8 +1,2 @@
 export { SplitButton, type SplitButtonProps } from './SplitButton';
-export {
-  getSplitButtonStyle,
-  splitButtonRuntime,
-  splitButtonSizeTokens,
-  type SplitButtonStyle,
-} from './SplitButton.defaults';
 export type { SplitButtonSize, SplitButtonVariant } from './SplitButton.types';
