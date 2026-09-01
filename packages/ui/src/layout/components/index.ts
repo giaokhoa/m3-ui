67285917e86cad32c213eee87779923e73e30d1f
Trần Kim Vũ <22520100117@uah.edu.vn>
2026-09-01 20:12:51 +0700

    feat(adaptive-navigation): add NavigationSuiteScaffold
    Closes #210

--- packages/ui/src/layout/components/index.ts
+++ packages/ui/src/layout/components/index.ts
@@ -1,4 +1,5 @@
 export * from './ListDetailPaneScaffold';
+export * from './NavigationSuiteScaffold';
 export * from './Scaffold';
 export * from './SupportingPaneScaffold';
 export * from './ThreePaneScaffold';
